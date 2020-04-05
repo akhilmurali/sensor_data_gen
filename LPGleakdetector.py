e31e97112e3279f8e4583c340006bb7ee7215abd
Akhil <github@akhilmurali.com>
2020-04-05 12:52:22 +0530

    Initial commit

---
+++ LPGleakdetector.py
@@ -0,0 +1,46 @@
+"""
+For Official Documentation and More Information Refer
+https://docs.python.org/3/library/socket.html
+
+OUTPUT FORMAT:
+<time>,<device-id>,<region>,<smoke>
+
+"""
+
+import socket
+import sys
+import time
+import random
+import math
+from datetime import datetime
+
+if len(sys.argv) < 5:
+    print("Format: <host> <port> <device-id> <region>", sys.argv[0])
+    exit(-1)
+
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+DEVICE_ID = sys.argv[3]
+REGION = sys.argv[4]
+LIMIT = 0.6
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    while True:
+        conn, addr = s.accept()
+        print("Connected to: {}".format(addr))
+        with conn:
+            while True:
+                try:
+                    leak = random.random() > LIMIT
+                    # time in milliseconds
+                    data = "{},{},{},{}".format(int(time.time()) * 1000, DEVICE_ID, REGION, leak)
+                    # data = "{},{},{},{}".format(datetime.now(), DEVICE_ID, REGION, smoke)
+                    print(data)
+                    conn.sendall("{}\n".format(data).encode('utf-8'))
+                    time.sleep(1)
+                except Exception as e:
+                    print(e)
+                    break
---
+++ humiditydetector.py
@@ -0,0 +1,45 @@
+"""
+
+OUTPUT FORMAT:
+<time>,<device-id>,<region>,<temp-value>
+
+"""
+
+import socket
+import sys
+import time
+import random
+import math
+from datetime import datetime
+
+if len(sys.argv) < 5:
+    print("Format: <host> <port> <device-id> <floor> [<humidity-mean>] [<humidity-std>]", sys.argv[0])
+    exit(-1)
+
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+DEVICE_ID = sys.argv[3]
+FLOOR = sys.argv[4]
+HUMIDITY_MEAN = sys.argv[5] if len(sys.argv) >= 6 else 27
+HUMIDITY_STD = sys.argv[6] if len(sys.argv) >= 7 else 30
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    while True:
+        conn, addr = s.accept()
+        print("Connected to: {}".format(addr))
+        with conn:
+            while True:
+                try:
+                    humidity = random.gauss(HUMIDITY_MEAN, HUMIDITY_STD)
+                    # time in milliseconds
+                    data = "{},{},{},{:.2f}".format(int(time.time()) * 1000, DEVICE_ID, FLOOR, humidity)
+                    # data = "{},{},{},{:.2f}".format(datetime.now(), DEVICE_ID, REGION, temp)
+                    print(data+"\n")
+                    conn.sendall("{}\n".format(data).encode('utf-8'))
+                    time.sleep(1)
+                except Exception as e:
+                    print(e)
+                    break
